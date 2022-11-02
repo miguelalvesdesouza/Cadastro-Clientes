c95913caf2dd57131788cb2d7345604cb7f962df
miguelalvesdesouza <miguelalvesdesouza1@gmail.com>
2022-11-02 00:01:59 -0300

    Arquitetando projeto front

--- SPA/CadastroClientes-App/src/app/app-routing.module.ts
+++ SPA/CadastroClientes-App/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClientesComponent } from './components/clientes/clientes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'clientes', component: ClientesComponent},
+  {path: '', redirectTo: 'clientes', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
